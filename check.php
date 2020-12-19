2b4c463444eb192f09edaab939446d1ba79c2d46
RussiaFPS <35640550+nikitakrut58@users.noreply.github.com>
2020-12-19 16:46:04 +0300

    design start page

--- check.php
+++ check.php
@@ -4,12 +4,33 @@ $name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
 $pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
 
 
+if (mb_strlen($login) < 3 || mb_strlen($login) > 90) {
+    echo "Недопустимая длина логина";
+    exit();
+} elseif (mb_strlen($name) < 3 || mb_strlen($name) > 50) {
+    echo "Недопустимая длина имени";
+    exit();
+} elseif (mb_strlen($pass) < 2 || mb_strlen($pass) > 15) {
+    echo "Недопустимая длина пароля (от 2 до 15 символов)";
+    exit();
+}
+
+
 $pass = md5($pass."QafjhgjgH74");
 
 $mysql = new mysqli('localhost', 'root', 'root', 'register-bd');
-$mysql->query("INSERT INTO `users` (`login` , `pass` , `name`)
-VALUES('$login','$pass','$name') ");
-$mysql->close();
 
-header('Location:/');
+
+$result = $mysql->query("SELECT * FROM `users` WHERE `login`='$login'");
+$user = $result->fetch_assoc();
+if(count($user)==0){
+  $mysql->query("INSERT INTO `users` (`login` , `pass` , `name`)
+  VALUES('$login','$pass','$name') ");
+  $mysql->close();
+  header('Location:/');
+}else{
+  echo "Такой пользователь уже зарегистрирован";
+  exit();
+}
+
 ?>
